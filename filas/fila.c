7839552420e8d98376b5dde28113ce0015c196bf
Alan Vinícius Sega <asega@br.ibm.com>
2018-04-10 16:10:06 -0300

    exercicios 1 e 2

--- filas/fila.c
+++ filas/fila.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// Estruturas para Fila
 typedef struct tipoElemento {
     int elemento;
 
@@ -44,14 +45,14 @@ int enqueue(Fila *f, int elemento) {
     }
 
     if(empty(f)) {
-        f->primeiro = elemento;
-        f->ultimo = elemento;
+        f->primeiro = e;
     } else {
         f->ultimo->prox = e;
-        e->elemento = elemento;
-        e->prox = NULL;
-        f->ultimo = e;
     }
+
+    f->ultimo = e;
+    e->elemento = elemento;
+    e->prox = NULL;
 
     return 1;
 }
@@ -62,16 +63,157 @@ int dequeue(Fila *f, int *elemento) {
     }
 
     *elemento = f->primeiro->elemento;
-    free(f->primeiro);
     f->primeiro = f->primeiro->prox;
+    return 1;
+}
+
+// Exericios de Fila
+
+// 1. Implementar a função print que imprime os elementos da fila
+int print(Fila *f) {
+    if (empty(f)) {
+        return 0;
+    }
+
+    elementoFila *e = (elementoFila *) malloc(sizeof(elementoFila));
+    e = f->primeiro;
+
+    while(e != NULL) {
+        printf("%d", e->elemento);
+        e = e->prox;
+        if (e != NULL) {
+            printf(", ");
+        }
+    }
+
+    printf("\n");
+    return 1;
+}
+
+// 2. Dada uma fila, construir um procedimento que elimina dessa fila os n primeiros elementos.
+// Prever a possibilidade da fila estar vazia e de não ter n elementos.
+int contaElementos(Fila *f) {
+    int qtd = 0;
+    elementoFila *e = f->primeiro;
+    while(e != NULL) {
+        qtd++;
+        e = e->prox;
+    }
+
+    return qtd;
+}
+
+int nDequeue(Fila *f, int n) {
+    if (empty(f)) {
+        return 0;
+    }
+    
+    int *i = malloc(sizeof(int));
+    elementoFila *e = (elementoFila *) malloc(sizeof(elementoFila));
+    e = f->primeiro;
+
+    // Verificando se a quantidade de elementos a se remover a maior do que a quantidade de elementos na fila.
+    int qtd = contaElementos(f);
+
+    // Se a quantidade de elementos for maior que o número a se deletar, programa para.
+    if (n > qtd) {
+        return 0;
+    } else {
+        while(n > 0) {
+            dequeue(f, i);
+            n--;
+        }
+    }
 
     return 1;
 }
 
-int print(Fila *f) {
-    elementoFila *e = (elementoFila *) malloc(sizeof(elementoFila));
-
-    while(e != NULL) {
-            
+/* 3. Dadas duas filas: Q1 e Q2, contendo números inteiros ordenados. Construir um procedimento que
+ * recebe as duas filas e faz a junção das duas gerando uma terceira. Essa terceira fila deverá ter
+ * seus elementos ordenados. As duas filas dadas Q1 e Q2 ficarão vazias ao final do processo.
+ */
+int ordenaFila(Fila *f) {
+    if (empty(f)) {
+        return 0;
     }
+
+    int qtd = contaElementos(f);
+    elementoFila *e = (elementoFila*) malloc(sizeof(elementoFila));
+    int maior = 0;
+
+    e = f->primeiro;
+    while(e != NULL) {
+        if (e->elemento > maior) {
+            maior = e->elemento;
+            //enqueue
+        }
+        e = e->prox;
+    }
+
+    return 1;
+}
+
+Fila* juntaFilas(Fila *q1, Fila *q2) {
+    if (empty(q1) || empty(q2)) {
+        exit(1);
+    }
+
+    Fila *q3 = (Fila*) malloc(sizeof(Fila));
+
+    // elementoFila *e = (elementoFila*) malloc(sizeof(elementoFila));
+    int *i = malloc(sizeof(int));
+
+    while(!empty(q1)) {
+        dequeue(q1, i);
+        enqueue(q3, *i);
+    }
+
+    while(!empty(q2)) {
+        dequeue(q2, i);
+        enqueue(q3, *i);
+    }
+
+    return q3;
+}
+
+
+// 4. Construir uma função que receba duas filas Q1 e Q2, contendo números inteiros ordenados e constrói
+// uma terceira fila, contendo os elementos da fila Q1 sem os elementos de interseção de Q1 com Q2.
+
+/* 5. Uma fila de prioridades é organizada de acordo com a prioridade (de 0 a 9) de um elemento. Desta forma, 
+ * o registro de dados deve conter o valor do elemento e sua prioridade. Elementos com prioridade 0 devem 
+ * estar no início da fila, seguidos dos elementos de prioridade 1, até que, por fim, estarão os elementos 
+ * de prioridade 9. Desta forma, ao adicionar um novo elemento na fila, este deverá ser adicionado após o 
+ * último elemento de mesma prioridade. Como a fila estará ordenada pela prioridade, a remoção não sofre 
+ * alteração. Implemente uma fila de prioridades, com as estruturas necessárias e as seguintes operações 
+ * init, empty, enqueue e dequeue.
+ */
+
+// Função main para testes
+int main() {
+    Fila *fila1 = init();
+    Fila *fila2 = init();
+    int i;
+
+    for(i=10; i>=1; i--) {
+        enqueue(fila1, i);
+    }
+    /*
+    for(i=11; i<=20; i++) {
+        enqueue(fila2, i);
+    }
+
+    elementoFila *e = (elementoFila*) malloc(sizeof(elementoFila));
+
+    printf("Fila 1:\n");
+    print(fila1);
+
+    printf("Fila 2:\n");
+    print(fila2);
+    */
+    printf("Fila 1:\n");
+    ordenaFila(fila1);
+    print(fila1);
+
+    return 0;
 }
